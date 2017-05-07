35e98a3a07f065000055b89c7c9e587681fbe0e8
cautree <yanyanliujs@gmail.com>
2017-05-07 14:34:34 -0400

    rendered template

--- simple_app.py
+++ simple_app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask import request
+from flask import render_template
+
 
 app= Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="Treehouse"):
-	name = request.args.get('name',name) # the first name is key from dict
-	return "hello from {}".format(name)
+	return render_template("index.html", name=name)
+
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+	context = {'num1':num1,'num2':num2}
+	return render_template("add.html", **context)
+	
+
 
 app.run(debug=True)
